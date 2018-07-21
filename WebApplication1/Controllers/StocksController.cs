84133de30c637b29093902808229e0f336c8db1d
Michael <mikeishere3@gmail.com>
2018-07-21 12:34:52 -0400

    first commit, created MVC web application

---
+++ WebApplication1/Controllers/StocksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcStock.Controllers
+{
+    public class StocksController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
